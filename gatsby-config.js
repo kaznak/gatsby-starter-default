2ea037c47cf38312c201b3a2a3f1a1603a909c03
kaznak <kaznak.at.work@gmail.com>
2020-05-06 22:59:57 +0900

    feat(/authors): add createPages example

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,10 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    authors: [
+      { name: "Gatsby Girl", slug: "gatsby-girl" },
+      { name: "Gatsby Guy", slug: "gatsby-guy" },
+    ],
   },
   plugins: [
     `gatsby-plugin-pnpm`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+"use strict"
+require("ts-node").register({
+  compilerOptions: {
+    module: "commonjs",
+    target: "esnext",
+  },
+})
+const gastbyNode = require("./src/gatsby-node/index")
+exports.createPages = gastbyNode.createPages
